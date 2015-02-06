1abf761f87fef44d9b9636d67456e4a2590d30a4
dcampagna <campagna@esteco.com>
2015-02-06 14:57:25 +0100

    Homepage module in protractor Tests

--- test/e2e/index-test.js
+++ test/e2e/index-test.js
@@ -2,32 +2,37 @@ describe('WebApp', function () {
 
     describe('index page', function () {
 
-        browser.get('http://localhost:9000');
+        var HomePage = require("./pages/home_page.js");
+        var page = new HomePage();
+
+        beforeEach(function() {
+            page.get();
+        });
 
         it('should show three products', function () {
-            expect(element.all(by.repeater('product in store.products')).count()).toEqual(3);
+            expect(page.productList().count()).toEqual(3);
         });
 
         it('should show ruby name and price', function () {
-            var ruby = element.all(by.repeater('product in store.products')).first();
-            expect(ruby.all(by.css('h3')).first().getText()).toBe('Ruby');
-            expect(ruby.all(by.css('h4')).first().getText()).toBe('$102.95');
+            var ruby = page.productList().first();
+            expect(page.title(ruby)).toBe('Ruby');
+            expect(page.price(ruby)).toBe('$102.95');
         });
 
-        it('should show ruby description', function() {
-            var ruby = element.all(by.repeater('product in store.products')).first();
-            var whiteframe = ruby.all(by.css('[ng-show="tab === 1"]')).first();
-            expect(whiteframe.isDisplayed()).toBeTruthy();
-            expect(whiteframe.all(by.css('p')).first().getText()).toBe('The reddest ruby in the World!');
+        it('should show ruby additionaInfo', function() {
+            var ruby = page.productList().first();
+            var whiteFrame = page.descriptionWhiteFrame(ruby);
+            expect(whiteFrame.isDisplayed()).toBeTruthy();
+            expect(page.additionalInfo(whiteFrame)).toBe('The reddest ruby in the World!');
         });
 
         it('should show ruby specifications', function () {
-            var ruby = element.all(by.repeater('product in store.products')).first();
-            var specButton = ruby.all(by.css('[ng-click="tab = 2"]')).first();
+            var ruby = page.productList().first();
+            var specButton = page.specButton(ruby);
             specButton.click();
-            var whiteframe = ruby.all(by.css('[ng-show="tab === 2"]')).first();
-            expect(whiteframe.isDisplayed()).toBeTruthy();
-            expect(whiteframe.all(by.css('p')).first().getText()).toBe('None yet');
+            var whiteFrame = page.specWhiteFrame(ruby);
+            expect(whiteFrame.isDisplayed()).toBeTruthy();
+            expect(page.additionalInfo(whiteFrame)).toBe('None yet');
         });
 
     });
---
+++ test/e2e/pages/home_page.js
@@ -0,0 +1,40 @@
+var HomePage = function HomePage() {
+    this.get = function () {
+        browser.get('/');
+    };
+
+    this.productList = function () {
+        return element.all(by.repeater('product in store.products'))
+    };
+
+    this.productTab = function (element, selector) {
+        return element.all(by.css(selector)).first();
+    };
+
+    this.title = function (productTab) {
+        return productTab.all(by.css('h3')).first().getText();
+    };
+
+    this.price = function (productTab) {
+        return productTab.all(by.css('h4')).first().getText();
+    };
+
+    this.descriptionWhiteFrame = function (productTab) {
+        return productTab.all(by.css('[ng-show="tab === 1"]')).first();
+    }
+
+    this.specWhiteFrame = function (productTab) {
+        return productTab.all(by.css('[ng-show="tab === 2"]')).first();
+    }
+
+    this.additionalInfo=function(whiteFrame){
+        return whiteFrame.all(by.css('p')).first().getText();
+    }
+
+    this.specButton=function(productTab){
+        return productTab.all(by.css('[ng-click="tab = 2"]')).first();
+    }
+
+};
+
+module.exports = HomePage;

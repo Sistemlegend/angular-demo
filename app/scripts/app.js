63d38234b2d524f11b091b89c7aa537a987e1cd1
dcampagna <campagna@esteco.com>
2015-02-09 15:49:23 +0100

    Add productDirectives module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,68 +1,8 @@
 (function () {
-    var app = angular.module('store', ['ngMaterial', 'productControllers']);
-
-    app.directive('productData', function() {
-        return {
-            restric: 'E',
-            templateUrl: 'templates/product-data.html',
-            scope: {
-                product: '='
-            }
-        };
-    });
-
-    app.directive('productPanels', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/product-panels.html',
-            scope: {
-                product: '='
-            },
-            controller: function() {
-                var tab = 1;
-
-                this.selectTab = function(value) {
-                    tab = value;
-                };
-
-                this.isSelected = function(value) {
-                    return tab == value;
-                };
-            },
-            controllerAs: 'panelCtrl'
-        };
-    });
-
-    app.directive('productReviews', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/product-reviews.html',
-            scope: {
-                product: '='
-            }
-        };
-    });
-
-    app.directive('reviewForm', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/review-form.html',
-            replace: true,
-            scope: {
-                product: '='
-            },
-            controller: function() {
-                this.review = {};
-                this.product;
-
-                this.addReview = function() {
-                    this.product.reviews.push(this.review);
-                    this.review = {};
-                };
-            },
-            controllerAs: 'reviewCtrl',
-            bindToController: true
-        };
-    });
+    var app = angular.module('store', [
+        'ngMaterial',
+        'productControllers',
+        'productDirectives'
+    ]);
 
 })();

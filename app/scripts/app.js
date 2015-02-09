94c60bde106a2777f51d36b7e8e70e289bf185ef
dcampagna <campagna@esteco.com>
2015-02-09 09:59:34 +0100

    Add methods to PanelController

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -42,5 +42,15 @@
 
     app.controller('PanelController', function() {
 
+        var tab = 1;
+
+        this.selectTab = function(value) {
+            tab = value;
+        };
+
+        this.isSelected = function(value) {
+            return tab == value;
+        };
+
     });
 })();
--- test/spec/PanelController.spec.js
+++ test/spec/PanelController.spec.js
@@ -13,4 +13,13 @@ describe('PanelController', function () {
         expect(panelController).toBeDefined();
     });
 
+    it('Should select tab 1 on startup', function() {
+        expect(panelController.isSelected(1)).toBeTruthy();
+    });
+
+    it('Should select tab 2', function() {
+        panelController.selectTab(2);
+        expect(panelController.isSelected(2)).toBeTruthy();
+    });
+
 });

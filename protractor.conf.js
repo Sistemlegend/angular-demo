094514e7ab5506e31e6cecedef35dc4885867997
dcampagna <campagna@esteco.com>
2015-02-09 09:44:53 +0100

    Protractor direct connect

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,15 +1,9 @@
-// An example configuration file.
-// https://raw.github.com/angular/protractor/master/example/conf.js
 exports.config = {
-    // The address of a running selenium server.
-    seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.44.0.jar', // Make use you check the version in the folder
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    // Capabilities to be passed to the webdriver instance.
+    directConnect: true,
     capabilities: {
         'browserName': 'chrome'
     },
     baseUrl: 'http://localhost:9000',
-    // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
         showColors: true,
         defaultTimeoutInterval: 30000

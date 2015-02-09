8fecdbe4d8e13f3a5d49ce90bdc8515413b62a55
dcampagna <campagna@esteco.com>
2015-02-09 12:55:42 +0100

    Add unit test for product-data directive

---
+++ test/spec/ProductData.spec.js
@@ -0,0 +1,26 @@
+describe('ProductData', function () {
+
+    var $compile, $rootScope;
+
+    beforeEach(module('store'));
+    beforeEach(module('templates'));
+
+    beforeEach(inject(function (_$compile_, _$rootScope_, $httpBackend) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        $httpBackend.whenGET(/.*/).respond();
+    }));
+
+    it('should replace the element with the appropriate content', function () {
+        $rootScope.product = {
+            name: 'Ruby',
+            image: '/images/ruby.png',
+            price: 102.95
+        };
+        var element = $compile("<product-data></product-data>")($rootScope);
+        $rootScope.$digest();
+        expect(element.find('h3').text()).toBe($rootScope.product.name);
+        expect(element.find('h4').text()).toBe('$' + $rootScope.product.price);
+    });
+
+});

03ab8a0ff0a9f97f9515b7579c9c32e9aff80d26
dcampagna <campagna@esteco.com>
2015-02-09 17:02:46 +0100

    Use ProductResource in StoreController

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,7 @@
     var app = angular.module('store', [
         'ngMaterial',
         'ngResource',
+        'productServices',
         'productControllers',
         'productDirectives'
     ]);
--- app/scripts/products/controllers.js
+++ app/scripts/products/controllers.js
@@ -1,71 +1,17 @@
 (function () {
 
-    var app = angular.module('productControllers', []);
+    var app = angular.module('productControllers', ['productServices']);
 
-    var gems = [
-        {
-            name: 'Ruby',
-            image: '/images/ruby.png',
-            price: 102.95,
-            description: 'The reddest ruby in the World!',
-            specifications: 'None yet',
-            canPurchase: true,
-            soldOut: false,
-            reviews: [
-                {
-                    stars: 5,
-                    body: 'I love this product!',
-                    author: 'pigna@esteco.com'
-                },
-                {
-                    stars: 1,
-                    body: 'This product sucks.',
-                    author: 'miotto@esteco.com'
-                }
-            ]
-        },
-        {
-            name: 'Diamond',
-            image: '/images/diamond.png',
-            price: 200,
-            description: 'Diamonds are girls best friends.',
-            specifications: 'None yet',
-            canPurchase: false,
-            soldOut: false,
-            reviews: [
-                {
-                    stars: 5,
-                    body: 'Cannot live without diamonds!',
-                    author: 'ricco@esteco.com'
-                }
-            ]
-        },
-        {
-            name: 'Emerald',
-            image: '/images/emerald.png',
-            price: 150.5,
-            description: 'Holy crap on a cracker, this emerald is ginormous!',
-            specifications: 'None yet',
-            canPurchase: true,
-            soldOut: false,
-            reviews: [
-                {
-                    stars: 3,
-                    body: 'Meh...',
-                    author: 'segatto@esteco.com'
-                },
-                {
-                    stars: 2,
-                    body: 'It\'s tiny!',
-                    author: 'piove@esteco.com'
-                }
-            ]
-        }
-    ];
+    app.controller('StoreController', function (ProductResource) {
 
-    app.controller('StoreController', function () {
+        this.products = [];
 
-        this.products = gems;
+        var that = this;
+        ProductResource.query().then(
+            function(data) {
+                that.products = data;
+            }
+        );
 
     });
 

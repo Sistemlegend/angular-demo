ff2125ee798df3aab6dd61e537e074fd2ee9a99a
dcampagna <campagna@esteco.com>
2015-02-16 14:59:59 +0100

    Fix productHeading directive

--- app/scripts/products/directives.js
+++ app/scripts/products/directives.js
@@ -4,7 +4,7 @@
 
     app.directive('productHeading', function() {
         return {
-            restric: 'E',
+            restrict: 'E',
             templateUrl: 'templates/product-heading.html',
             scope: {
                 product: '='
